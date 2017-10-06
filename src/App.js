3143e9f1a879e9f8a0d3a007dd25ccfdd3bcc2ea
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2017-10-06 18:29:18 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+// import CardsList from './components/CardsList';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        {/* <CardsList /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
